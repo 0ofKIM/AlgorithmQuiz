37f4936551826c3ec346dc7bce5495514b51308e
uykim3 <uykim3@lotte.net>
2020-08-31 23:21:51 +0900

    누락 코드 추가

--- Sources/Sorting/LargestNumber.swift
+++ Sources/Sorting/LargestNumber.swift
@@ -1,2 +1,11 @@
+/**
+1) 프로그래머스
+2) Level2 문제 : 가장 큰 수
+*/
 import Foundation
 
+func solution(_ numbers:[Int]) -> String {
+    var answer = numbers.sorted(by: { Int("\($0)\($1)")! > Int("\($1)\($0)")! })
+
+    return answer.first == 0 ? "0" : answer.reduce("") { String($0) + String($1) }
+}
