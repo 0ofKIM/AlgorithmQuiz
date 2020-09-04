c8fbed6f265ced61059323bd814633e8c44551b9
0ofKim <>
2020-09-04 23:42:59 +0900

    가장 큰 수 저장

--- AlgorithmQuiz2.playground/Contents.swift
+++ AlgorithmQuiz2.playground/Contents.swift
@@ -1,7 +1,19 @@
-import UIKit
+import Foundation
 
-func solution(_ strings:[String], _ n:Int) -> [String] {
+func solution(_ number:String, _ k:Int) -> String {
+    var answer = number
+    let answerCount = number.count-k
     
+//    while answer.count > answerCount {
+        let kIndex = answer.index(answer.startIndex, offsetBy: answerCount-1)
+        
+        let maxNum = answer[answer.startIndex...kIndex].max()!
+        let maxIndex = answer.firstIndex(of: maxNum)!
+        
+    answer[..<maxIndex].sorted(by: <)
+    answer
+//    }
+    return answer
 }
 
-solution(["abce", "abcd", "cdx"], 2)
+solution("4177252841", 4)
